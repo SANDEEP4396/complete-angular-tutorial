9e02b6e46a6dd9707fa878a4664c0894d0d6b653
Sandeep Sagar <sandeep.sagar033@gmail.com>
2025-07-30 19:45:52 -0500

    Implement tasks feature with dynamic task rendering and styling; add dummy tasks data and refactor task component structure.

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,12 +1,20 @@
 import { Component, Output, Input, EventEmitter } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
+import { Task } from "./task/task";
+import { dummyTasks } from '../dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [Task],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css'
 })
 export class Tasks {
+@Input({required:true}) userId!: string;
 @Input({required:true}) name: string | undefined;
+tasks = dummyTasks;
+
+get selectedUserTasks() {
+  return this.tasks.filter(task => task.userId === this.userId);
+}
 }
