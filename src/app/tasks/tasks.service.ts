03b365ddd7e18b352db143c265e54f37f966318f
Sandeep Sagar <sandeep.sagar033@gmail.com>
2025-08-01 18:39:00 -0500

    Completed Module 1

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -7,23 +7,37 @@ import { TaskType } from "./task/taskType.model";
 export class TasksService{
     private tasks = dummyTasks;
 
+    constructor() {
+        const tasksFromLocalStorage = localStorage.getItem('tasks');
+        if (tasksFromLocalStorage) {
+            this.tasks = JSON.parse(tasksFromLocalStorage);
+        } else {
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
+        }
+    }   
   getTasksByUserId(userId: string) {
     return this.tasks.filter(task => task.userId === userId);
   }
 
     addTask(taskData: NewTaskType, userId: string): void {
-          const newTask: TaskType = {
-            id: Math.random().toString(),
+        const newTask: TaskType = {
+            id: 't' + Date.now() + Math.floor(Math.random() * 100),
             userId: userId,
             title: taskData.title,
             summary: taskData.summary,
             dueDate: taskData.dueDate
-          };
+        };
         
           this.tasks.push(newTask);
+          this.saveTasksToLocalStorage();
     }
 
     removeTask(taskId: string): void {
         this.tasks = this.tasks.filter(task => task.id !== taskId);
+        this.saveTasksToLocalStorage();
+    }
+
+    private saveTasksToLocalStorage() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
 }
